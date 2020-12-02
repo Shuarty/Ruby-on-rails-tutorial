2c8190e8b0441dba794e9db2d32ec87989c3af45
Artem Shulga <Shuarty93@gmail.com>
2020-12-02 19:00:17 +0300

    pagination per page 10

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,7 +1,7 @@
 # phrases controller
 class PhrasesController < ApplicationController
   def index
-    @phrases = Phrase.paginate(page: params[:page])
+    @phrases = Phrase.paginate(page: params[:page], per_page: 10)
   end
 
   def new
